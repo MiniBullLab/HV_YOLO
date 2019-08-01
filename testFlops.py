8f64ca551f17a1ef61fa762780a90454fd2bf913
lpj <1014153254@qq.com>
2019-08-01 21:11:34 +0800

    lipeijie add FLOPS

---
+++ testFlops.py
@@ -0,0 +1,6 @@
+import torch
+from utils.model_summary import summary
+from model.mobileV2FCN import *
+
+model = MobileFCN("./cfg/mobileFCN.cfg", img_size=[640, 352])
+summary(model, [1, 3, 640, 352])
